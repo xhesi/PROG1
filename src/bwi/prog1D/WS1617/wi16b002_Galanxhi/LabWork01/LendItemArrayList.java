c9532a0d561f1c42a2c59304a803d78b945da242
Developer Beder <developer@beder.edu.al>
2016-12-20 23:39:42 +0100

    fixed error regarding lendItems

--- src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/LendItemArrayList.java
+++ src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/LendItemArrayList.java
@@ -1,8 +1,9 @@
 package bwi.prog1D.WS1617.wi16b002_Galanxhi.LabWork01;
+import java.util.ArrayList;
 
 public class LendItemArrayList {
-	static int INITIAL_SIZE = 20;
-	static boolean resizable = false;
-	static LendItem[] lendItems;
-	static int next=0;
+	int INITIAL_SIZE = 20;
+	boolean resizable = false;
+	LendItem[] lendItems;
+	int next = 0;
 }
