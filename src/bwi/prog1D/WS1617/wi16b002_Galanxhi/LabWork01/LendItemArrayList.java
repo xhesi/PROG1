15bbe6c3c7e49b4d1ac4a5339eac608ae9d164ed
Developer Beder <developer@beder.edu.al>
2016-12-20 22:23:27 +0100

    Revert "implemented *add* function and modified LendItemArrayList"
    This reverts commit b03cc6f8486a56583d0619e344312c2cfe2fd1b0.

--- src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/LendItemArrayList.java
+++ src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/LendItemArrayList.java
@@ -1,8 +1,9 @@
 package bwi.prog1D.WS1617.wi16b002_Galanxhi.LabWork01;
+import java.util.List;
 
 public class LendItemArrayList {
-	int INITIAL_SIZE = 20;
-	boolean resizable = false;
-	LendItem[] lendItems;
-	int next=0;
+	int INITIAL_SIZE;
+	boolean resizable;
+	LendItem[] landItems;
+	int next;
 }
