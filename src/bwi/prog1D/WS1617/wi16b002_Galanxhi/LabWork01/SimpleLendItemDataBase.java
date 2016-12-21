aad1ad00e7691e43d3f8e6b49d6e712490eb977a
Developer Beder <developer@beder.edu.al>
2016-12-21 02:04:59 +0100

    added case 5

--- src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/SimpleLendItemDataBase.java
+++ src/bwi/prog1D/WS1617/wi16b002_Galanxhi/LabWork01/SimpleLendItemDataBase.java
@@ -88,6 +88,22 @@ public class SimpleLendItemDataBase {
 					 TextIO.putln("LendItem not found (ID "+id+")");
 				 break;
 			 case 4:
+				 String[] sortOptions = {"lend date", "return date", "lender", "owner", "description"}
+				 for(int i =1; i<sortOptions.length;i++){
+					 TextIO.put(i%sortOptions.length+") by "+sortOptions[i-1]);
+				 }
+				 
+				 int chosenOption = TextIO.getInt();
+				 
+				 if(chosenOption > 4 && chosenOption < 0){
+					 TextIO.put("invalid choice");
+					 break;
+				 }
+				 
+				 LendItemArrayListFunctions.sort(list, chosenOption);
+				 
+				 LendItemArrayListFunctions.list(list, 1);
+				 
 				 break;
 			 case 5:
 				 TextIO.put("enter description: ");
